281e4302ba64e4a02f1a43da6bed5439a2083bbd
Prachi24599 <prachi24599@gmail.com>
2026-09-20 12:32:59 +0200

    download resume button added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import Button from './Button'
 import Container from './Container'
 import './Navbar.css'
 
@@ -27,7 +26,16 @@ function Navbar() {
           </ul>
         </nav>
 
-        <Button>Let's Talk</Button>
+        <a className="button button--primary navbar__resume" href="/resume.pdf" download>
+          <svg
+            className="navbar__resume-icon"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+          >
+            <path d="M12 3v11m0 0 4-4m-4 4-4-4M5 17v2h14v-2" />
+          </svg>
+          Resume
+        </a>
       </Container>
     </header>
   )
